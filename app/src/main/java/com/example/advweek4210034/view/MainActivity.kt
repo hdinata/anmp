404258fc1f832f2e917b4ad7366b3f47ecf497d0
Hendra <hdinata@gmail.com>
2024-04-04 14:04:21 +0700

    init

---
+++ app/src/main/java/com/example/advweek4210034/view/MainActivity.kt
@@ -0,0 +1,93 @@
+package com.example.advweek4210034.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.core.app.NotificationCompat
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
+import com.example.advweek4210034.R
+import com.example.advweek4210034.databinding.ActivityMainBinding
+import android.Manifest
+import android.content.pm.PackageManager
+import android.util.Log
+import androidx.core.app.ActivityCompat
+import androidx.core.app.NotificationManagerCompat
+import com.example.advweek4210034.util.createNotificationChannel
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
+
+    init {
+        instance = this
+    }
+
+    companion object {
+        private var instance:MainActivity ?= null
+
+        fun showNotification(title:String, content:String, icon:Int) {
+            val channelId = "${instance?.packageName}-${instance?.getString(R.string.app_name)}"
+            val notificationBuilder = NotificationCompat.Builder(instance!!.applicationContext, channelId).apply {
+                setSmallIcon(icon)
+                setContentTitle(title)
+                setContentText(content)
+                setStyle(NotificationCompat.BigTextStyle())
+                priority = NotificationCompat.PRIORITY_DEFAULT
+                setAutoCancel(true)
+            }
+
+            val notificationManager = NotificationManagerCompat.from(instance!!.applicationContext.applicationContext!!)
+
+            if (ActivityCompat.checkSelfPermission(instance!!.applicationContext, Manifest.permission.POST_NOTIFICATIONS) != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(instance!!,
+                    arrayOf(Manifest.permission.POST_NOTIFICATIONS),1)
+                return
+            }
+            notificationManager.notify(1001, notificationBuilder.build())
+        }
+
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when(requestCode) {
+            1 -> {
+                if(grantResults.isNotEmpty() && grantResults[0]== PackageManager.PERMISSION_GRANTED)
+                {
+                    Log.d("permission", "granted")
+                    //createNotifChannel(this, NotificationManagerCompat.IMPORTANCE_DEFAULT, false,
+                    //    "Notification channel for creating new student")
+
+                } else {
+                    Log.d("permission", "not granted")
+                }
+                return
+            }
+        }
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        navController = (supportFragmentManager.findFragmentById(R.id.fragmentHost) as NavHostFragment).navController
+        NavigationUI.setupActionBarWithNavController(this, navController)
+
+        // slide 34 :
+        createNotificationChannel(this,
+            NotificationManagerCompat.IMPORTANCE_DEFAULT, false,
+            getString(R.string.app_name), "App notification channel.")
+
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp()
+    }
+}
---
+++ app/src/main/java/com/example/advweek4210034/view/StudentListFragment.kt
@@ -0,0 +1,83 @@
+package com.example.advweek4210034.view
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.advweek4210034.R
+import com.example.advweek4210034.databinding.FragmentStudentListBinding
+import com.example.advweek4210034.viewmodel.ListViewModel
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [StudentListFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class StudentListFragment : Fragment() {
+    private lateinit var viewModel:ListViewModel
+    private val studentListAdapter  = StudentListAdapter(arrayListOf())
+    private lateinit var binding: FragmentStudentListBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        binding = FragmentStudentListBinding.inflate(inflater,container, false)
+        return binding.root
+    }
+
+    // slide 59
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        viewModel = ViewModelProvider(this).get(ListViewModel::class.java)
+        viewModel.refresh()
+
+        binding.recView.layoutManager = LinearLayoutManager(context)
+        binding.recView.adapter = studentListAdapter
+
+        observeViewModel()
+
+        binding.refreshLayout.setOnRefreshListener {
+            binding.recView.visibility = View.GONE
+            binding.txtError.visibility = View.GONE
+            binding.progressLoad.visibility = View.VISIBLE
+            viewModel.refresh()
+            binding.refreshLayout.isRefreshing = false
+
+        }
+    }
+
+    fun observeViewModel() {
+        // slide 61
+        viewModel.studentsLD.observe(viewLifecycleOwner, Observer {
+
+            studentListAdapter.updateStudentList(it)
+        })
+        // slide 62
+        viewModel.studentLoadErrorLD.observe(viewLifecycleOwner, Observer {
+            if(it == true) {
+                binding.txtError?.visibility = View.VISIBLE
+            } else {
+                binding.txtError?.visibility = View.GONE
+            }
+        })
+        // slide 63
+        viewModel.loadingLD.observe(viewLifecycleOwner, Observer {
+            if(it == true) {
+                binding.recView.visibility = View.GONE
+                binding.progressLoad.visibility = View.VISIBLE
+            } else {
+                binding.recView.visibility = View.VISIBLE
+                binding.progressLoad.visibility = View.GONE
+            }
+        })
+    }
+
+
+}
